ff4167d424fdcb524126b875d6dd0b9cdc19568a
zxy <358996085@qq.com>
2021-05-12 21:23:59 +0800

    Flex布局

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,53 @@ import {
   HashRouter as Router,
   Route,Link,Redirect, Switch,
 } from 'react-router-dom'
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  border: 1px solid red;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.div`
+  border: 1px solid green;
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+const Nav = styled.nav`
+  border: 1px solid blue;
+  > ul {
+    display: flex;
+    > li {
+      width: 33.33%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
 
 const App = () => (
   <Router>
-    <div>
-      <ul>
-        <li><Link to="/tags">标签页</Link></li>
-        <li><Link to="/money">记账页</Link></li>
-        <li><Link to="/statistics">统计页</Link></li>
-      </ul>
-
-      <hr/>
-      <Switch>
-        <Route path="/tags" component={Tags}/>
-        <Route path="/money" component={Money}/>
-        <Route path="/statistics" component={Statistics}/>
-        <Redirect exact path="/" to="/money" />
-        <Route component={NoMatch}/>
-      </Switch>
-    </div>
+    <Wrapper>
+      <Main>
+        <Switch>
+          <Route path="/tags" component={Tags}/>
+          <Route path="/money" component={Money}/>
+          <Route path="/statistics" component={Statistics}/>
+          <Redirect exact path="/" to="/money" />
+          <Route component={NoMatch}/>
+        </Switch>
+      </Main>
+      <Nav>
+        <ul>
+          <li><Link to="/tags">标签页</Link></li>
+          <li><Link to="/money">记账页</Link></li>
+          <li><Link to="/statistics">统计页</Link></li>
+        </ul> 
+      </Nav>
+    </Wrapper>
   </Router>
 )
 

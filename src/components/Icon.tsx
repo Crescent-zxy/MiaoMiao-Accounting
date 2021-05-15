80e468e5039779b6e4a95fd95359e1748090cb14
zxy <358996085@qq.com>
2021-05-15 22:03:17 +0800

    activeClassName

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,7 +1,7 @@
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
   requireContext.keys().forEach(requireContext);
 try {
-  importAll(require.context("../assets/icons", true, /\.svg$/));
+  importAll(require.context("../icons", true, /\.svg$/));
 } catch (error) {
   console.log(error);
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,22 +1,30 @@
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import Icon from './Icon';
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import Icon from "./Icon";
 
 const NavWrapper = styled.nav`
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   > ul {
     display: flex;
     > li {
       width: 33.33%;
       text-align: center;
-      padding: 5px 0;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      .icon{
-        width: 24px;
-        height: 24px;
+      > a {
+        padding: 5px 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .icon {
+          width: 24px;
+          height: 24px;
+        }
+        &.selected {
+          color: red;
+          .icon {
+            fill: red;
+          }
+        }
       }
     }
   }
@@ -26,19 +34,25 @@ const Nav = () => (
   <NavWrapper>
     <ul>
       <li>
-        <Icon name="tag" />
-        <Link to="/tags">标签</Link>
+        <NavLink to="/tags" activeClassName="selected">
+          <Icon name="tag" />
+          标签
+        </NavLink>
       </li>
       <li>
-        <Icon name="money" />
-        <Link to="/money">记账</Link>
-        </li>
-      <li>
-        <Icon name="chart" />
-        <Link to="/statistics">统计</Link>
+        <NavLink to="/money" activeClassName="selected">
+          <Icon name="money" />
+          记账
+        </NavLink>
       </li>
-    </ul> 
+      <li>
+        <NavLink to="/statistics" activeClassName="selected">
+          <Icon name="chart" />
+          统计
+        </NavLink>
+      </li>
+    </ul>
   </NavWrapper>
-)
+);
 
-export default Nav
+export default Nav;

5acd86fbe2fc2ed71de832da8050f251de98bd41
zxy <358996085@qq.com>
2021-05-16 17:28:05 +0800

    记账页模块化

---
+++ src/views/money/NoteSection.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const NoteSection = styled.section`
+  padding: 0 16px;
+  font-size: 14px;
+  > label {
+    display: flex;
+    align-items: center;
+    > span {
+      margin-right: 16px;
+      white-space: nowrap;
+    }
+    > input {
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
+`;
+export { NoteSection };
---
+++ src/views/money/TagSection.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+const TagSection = styled.section`
+  background-color: #fff;
+  padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  > ol {
+    margin: 0 -12px;
+    > li {
+      background-color: #d9d9d9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
+    }
+  }
+  > button {
+    background: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
+    margin-top: 8px;
+  }
+`;
+
+export { TagSection };

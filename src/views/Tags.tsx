7e7f660fc9e63621fd7dd88bbb3eca0169c83816
zxy <358996085@qq.com>
2021-05-20 16:02:23 +0800

    TagList 样式

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,15 +1,51 @@
 import Layout from "components/Layout";
+import Icon from "components/Icon";
 import { useTags } from "hooks/useTags";
+import styled from "styled-components";
 
+const TagList = styled.ol`
+  font-size: 16px;
+  background-color: white;
+  > li {
+    border-bottom: 1px solid #d5d5d5;
+    margin: 0 16px;
+    padding: 12px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  font-size: 16px;
+  border: none;
+  padding: 8px 12px;
+  background: #767676;
+  border-radius: 4px;
+  color: white;
+  margin-top: 50px;
+`;
 const Tags = () => {
   const { tags, setTags } = useTags();
   return (
     <Layout>
-      <ol>
+      <TagList>
         {tags.map((tag) => (
-          <li key={tag}>{tag}</li>
+          <li key={tag}>
+            <span className="oneLine">{tag}</span>
+            <Icon name="right" />
+          </li>
         ))}
-      </ol>
+      </TagList>
+      <Center>
+        <Button>新增标签</Button>
+      </Center>
     </Layout>
   );
 };
